eddbb190a557470a592c744fbfa41fef76cd15e2
Joabe Levi <joabelevi@gmail.com>
2022-06-10 17:34:01 -0300

    alterando views para serem baseadas em classes

--- minha_dashboard/urls.py
+++ minha_dashboard/urls.py
@@ -2,9 +2,9 @@ from django.urls import URLPattern, path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('total_vendido/', views.retorna_total_vendido, name='retorna-total-vendido'),
-    path('relatorio_faturamento/', views.relatorio_faturamento, name='relatorio-faturamento'),
-    path('relatorio_produtos/', views.relatorio_produtos, name='relatorio-produto'),
-    path('relatorio_funcionario/', views.relatorio_funcionario, name='relatorio-funcionario')
+    path('', views.Home.as_view(), name='home'),
+    path('total_vendido/', views.RetornaTotalVendido.as_view(), name='retorna-total-vendido'),
+    path('relatorio_faturamento/', views.RelatorioFaturamento.as_view(), name='relatorio-faturamento'),
+    path('relatorio_produtos/', views.RelatorioProdutos.as_view(), name='relatorio-produto'),
+    path('relatorio_funcionario/', views.RelatorioFuncionario.as_view(), name='relatorio-funcionario')
 ]
